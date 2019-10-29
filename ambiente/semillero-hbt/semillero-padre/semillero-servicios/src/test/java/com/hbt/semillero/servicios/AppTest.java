d3592057bd715742c5256105f9bec85b1d59377a
juanmalay <56891160+juanmalay@users.noreply.github.com>
2019-10-29 11:33:11 -0500

    agruegue el metodo test para para probar el invertir cadena

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,46 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
+	}
+	/**
+	 * Metodo que permite validar si dada la suma de dos numero el resultado es el
+	 * correcto
+	 */
+	@Test
+	public void primeraPU() {
+		Long resultadoEsperado = 2159L;
+		Long sumando1 = 1500L;
+		Long sumando2 = 659L;
+		Long resultado = sumando1 + sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+	}
+
+	/**
+	 * 
+	 * Metodo encargado de dada una cadena invertir su posicion y retornarla al
+	 * revez
+	 * 
+	 * @param cadena
+	 * @return
+	 */
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+		return cadenaInvertida;
+	}
+	
+	@Test
+	public void sundaPrueba() {
+		String resultadoEsperado = "leunaM";
+		String resultado = invertirCadena("Manuel");
+		Assert.assertEquals(resultado, resultadoEsperado);
 	}
 }
